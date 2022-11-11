60bce34c622f68aebbd989a95595b998a1c264ea
0808javierramirez88@gmail.com <0808javierramirez88@gmail.com>
2022-11-11 18:55:34 -0500

    perfil

---
+++ app/src/main/java/com/example/latinshopapp/view/ui/activities/fragments/PerfilFragment.kt
@@ -0,0 +1,23 @@
+package com.example.latinshopapp.view.ui.activities.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import com.example.latinshopapp.R
+
+class PerfilFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_perfil, container, false)
+    }
+   }
