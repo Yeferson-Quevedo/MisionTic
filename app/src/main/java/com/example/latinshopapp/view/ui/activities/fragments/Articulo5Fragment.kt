9839ce95d666b702c68648d96061389fd3b5e40d
Diana <arizagironza@gmail.com>
2022-11-14 14:55:23 -0500

    Se agregaron 6 fragments de la descripción de los artículos Ellos Se borraron algunos fragments que ya no se usaban como el wishlist

--- app/src/main/java/com/example/latinshopapp/view/ui/activities/fragments/Articulo5Fragment.kt
+++ app/src/main/java/com/example/latinshopapp/view/ui/activities/fragments/Articulo5Fragment.kt
@@ -1,10 +1,11 @@
-package com.example.latinshopapp
+package com.example.latinshopapp.view.ui.activities.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.latinshopapp.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ellos2.newInstance] factory method to
+ * Use the [Articulo5Fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ellos2 : Fragment() {
+class Articulo5Fragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class ellos2 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ellos2, container, false)
+        return inflater.inflate(R.layout.fragment_articulo5, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class ellos2 : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ellos2.
+         * @return A new instance of fragment Articulo5Fragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ellos2().apply {
+            Articulo5Fragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/example/latinshopapp/view/ui/activities/fragments/UbicacionFragment.kt
+++ app/src/main/java/com/example/latinshopapp/view/ui/activities/fragments/UbicacionFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.latinshopapp.R
 
-class AyudaFragment : Fragment() {
+class UbicacionFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
