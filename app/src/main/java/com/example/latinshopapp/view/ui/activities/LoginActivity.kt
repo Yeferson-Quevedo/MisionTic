90ebe0c8ee5fb24e5f257cbfe16c1e95f5fadb8b
yeferson <fenix0160@hotmail.com>
2022-11-18 09:16:28 -0500

    borrando productos y login

--- app/src/main/java/com/example/latinshopapp/view/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/latinshopapp/view/ui/activities/LoginActivity.kt
@@ -1,15 +1,9 @@
 package com.example.latinshopapp.view.ui.activities
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 import androidx.fragment.app.FragmentManager
 //import com.example.latinshopapp.HomeFragment
@@ -19,42 +13,25 @@ class LoginActivity : AppCompatActivity() {
     lateinit var Iniciobutton: Button
     lateinit var Registrobutton: TextView
     lateinit var Restaurarbutton: TextView
-    private lateinit var firebaseAuth: FirebaseAuth
-    @SuppressLint("MissingInflatedId")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        firebaseAuth=Firebase.auth
-        val email=findViewById<EditText>(R.id.loginemail)
-        val password=findViewById<EditText>(R.id.loginpassword)
         Iniciobutton=findViewById<Button>(R.id.iniciobutton)
         Registrobutton=findViewById<TextView>(R.id.registrobutton)
         Restaurarbutton=findViewById<TextView>(R.id.restaurarbutton)
-
         Registrobutton.setOnClickListener(){
             startActivity(Intent(this,RegisterActivity::class.java))
         }
-
         Iniciobutton.setOnClickListener(){
-            login(email.text.toString(), password.text.toString())
+
+            startActivity(Intent(this,HomeActivity::class.java))
         }
         Restaurarbutton.setOnClickListener(){
             startActivity(Intent(this,RestoreActivity::class.java))
         }
     }
 
-    private fun login(email:String, password:String){
-        firebaseAuth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this){
-                task->if(task.isSuccessful){
-                    val user=firebaseAuth.currentUser
-                Toast.makeText(baseContext, user?.uid.toString(), Toast.LENGTH_SHORT).show()
-                startActivity(Intent(this,HomeActivity::class.java))
-            }else{
-                Toast.makeText(baseContext, "Error en usuario y contraseña, por favor verifique sus datos", Toast.LENGTH_SHORT).show()
-            }
-            }
-    }
 
 }
--- app/src/main/java/com/example/latinshopapp/view/ui/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/latinshopapp/view/ui/activities/RegisterActivity.kt
@@ -4,37 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import com.example.latinshopapp.R
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var registrobutton: Button
-    private lateinit var firebaseAuth: FirebaseAuth
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-        firebaseAuth= Firebase.auth
         registrobutton=findViewById(R.id.registrobutton)
-        val email=findViewById<EditText>(R.id.registroemail)
-        val password = findViewById<EditText>(R.id.registropassword)
         registrobutton.setOnClickListener(){
-            crearcuenta(email.text.toString(), password.text.toString())
+            startActivity(Intent(this,HomeActivity::class.java))
         }
-    }
-    private fun crearcuenta(email:String,password:String){
-        firebaseAuth.createUserWithEmailAndPassword(email,password)
-            .addOnCompleteListener(this){
-                Task->if(Task.isSuccessful){
-                    Toast.makeText(baseContext, "Tu cuenta está creada", Toast.LENGTH_SHORT).show()
-                    startActivity(Intent(this,HomeActivity::class.java))
-                }else{
-                    Toast.makeText(baseContext,"Error en la creación de tu cuenta", Toast.LENGTH_SHORT).show()
-                }
-            }
     }
 }
