2b1ebf43cbc3127eb351887c480fe2b4283bbeea
yeferson <fenix0160@hotmail.com>
2022-11-18 11:54:31 -0500

    integrando creacion de usuario

--- app/src/main/java/com/example/latinshopapp/view/ui/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/latinshopapp/view/ui/activities/RegisterActivity.kt
@@ -4,16 +4,37 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.example.latinshopapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var registrobutton: Button
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        firebaseAuth= Firebase.auth
         registrobutton=findViewById(R.id.registrobutton)
+        val email=findViewById<EditText>(R.id.editTextEmail)
+        val password = findViewById<EditText>(R.id.editTextPassword)
         registrobutton.setOnClickListener(){
-            startActivity(Intent(this,HomeActivity::class.java))
+            crearcuenta(email.text.toString(), password.text.toString())
         }
+    }
+    private fun crearcuenta(email:String,password:String){
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){
+                    Task->if(Task.isSuccessful){
+                Toast.makeText(baseContext, "Tu cuenta está creada", Toast.LENGTH_SHORT).show()
+                //startActivity(Intent(this,HomeActivity::class.java))
+            }else{
+                Toast.makeText(baseContext,"Error en la creación de tu cuenta", Toast.LENGTH_SHORT).show()
+            }
+            }
     }
 }
